e629665dc064bb3c8dbd0c13f9694e599a8a8e23
GTourinho <gabrieltourinho@hotmail.com>
2022-08-12 12:53:05 -0300

    NavigationBloc Test

--- test/navigation_test.dart
+++ test/navigation_test.dart
@@ -1,1 +1,15 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:myapp/bloc/navigation/blocs.dart';
+
+main() {
+  blocTest<NavigationBloc, NavigationState>(
+      'Emits [NavigationState] when NavigateTo(NavItem.home) is added',
+      build: () =>
+          NavigationBloc(const NavigationState(currentNavItem: NavItem.home)),
+      act: (bloc) async =>
+          bloc.add(const NavigateTo(destination: NavItem.home)),
+      verify: (bloc) async {
+        expect(bloc.state.currentNavItem, NavItem.home);
+      });
+}
