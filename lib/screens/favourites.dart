393f4285ace633d3a59cfaffc8702646382829f4
GTourinho <gabrieltourinho@hotmail.com>
2022-08-13 18:14:37 -0300

    Favourites header added

--- lib/screens/favourites.dart
+++ lib/screens/favourites.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/custom_icons_icons.dart';
 import '../bloc/cep/blocs.dart';
 import '../widgets/bottom_navigation.dart';
 
@@ -16,11 +19,62 @@ class _FavouritesState extends State<Favourites> {
     return Scaffold(
       extendBodyBehindAppBar: true,
       body: Stack(
-        children: const [
-          FavouritesList(),
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(255, 180, 166, 255),
+                Colors.white,
+              ],
+            )),
+          ),
+          const FavouritesHeaderIcon(),
+          const FavouritesHeaderText(),
+          // FavouritesList(),
         ],
       ),
       bottomNavigationBar: const BottomNavigation(),
+    );
+  }
+}
+
+class FavouritesHeaderIcon extends StatelessWidget {
+  const FavouritesHeaderIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: ScreenUtil().setWidth(30.56),
+      top: ScreenUtil().setHeight(46.94),
+      child: Icon(
+        CustomIcons.star,
+        color: const Color.fromARGB(255, 109, 81, 255),
+        size: ScreenUtil().setHeight(32.88),
+      ),
+    );
+  }
+}
+
+class FavouritesHeaderText extends StatelessWidget {
+  const FavouritesHeaderText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: ScreenUtil().setHeight(93),
+      left: ScreenUtil().setWidth(32),
+      child: Text(
+        'Meus favoritos',
+        style: GoogleFonts.poppins(
+          fontSize: ScreenUtil().setSp(27),
+          fontWeight: FontWeight.w600,
+          color: const Color.fromARGB(255, 109, 81, 255),
+          height: 1.125,
+        ),
+      ),
     );
   }
 }
